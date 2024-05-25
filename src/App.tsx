b8e0c75d2241182f27c20e5689717629b134c6b1
Prashant Varma <prashantvarma5083@gmail.com>
2024-05-25 09:51:22 +0530

    contract done need to deploy

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import TextReveal from "./components/text-reveal";
+function App() {
+
+  return (
+    <>
+      <TextReveal text="Magic UI will change the way you design." />
+    </>
+  )
+}
+
+export default App
